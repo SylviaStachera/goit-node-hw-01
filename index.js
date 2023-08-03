0d4fbe29e7704e4723958ee8878c9c3491483ef5
Sylvia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2023-08-03 08:26:19 +0200

    In the index.js file, the 'commander' module was imported for convenient parsing of command-line arguments.

--- index.js
+++ index.js
@@ -1,53 +1,40 @@
-const { resolve } = require("node:path");
+// const { resolve } = require("node:path");
+const { Command } = require("commander");
 const { listContacts, getContactById, removeContact, addContact } = require("./contacts.js");
-const readline = require("node:readline");
 
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-});
+const program = new Command();
+program
+	.option("-a, --action <type>", "choose action")
+	.option("-i, --id <type>", "user id")
+	.option("-n, --name <type>", "user name")
+	.option("-e, --email <type>", "user email")
+	.option("-p, --phone <type>", "user phone");
 
-async function prompt(question) {
-	return new Promise((resolve) => {
-		rl.question(question + "\n >", (answer) => {
-			resolve(answer.trim());
-		});
-	});
-}
+program.parse(process.argv);
 
-async function test() {
-	try {
-		// Wyświetlanie wszystkich kontaktów
-		const listStartTime = Date.now();
-		await listContacts();
-		console.log("Czas wyświetlenia listy kontaktów: ", Date.now() - listStartTime, "ms");
+const argv = program.opts();
 
-		// Wyszukiwanie kontaktu po ID
-		const contactId = await prompt("Podaj nr id który chcesz wyszukać: ");
-		const findStartTime = Date.now();
-		const contacts = await getContactById(contactId);
-		console.log("Czas wyszukiwania kontaków: ", Date.now() - findStartTime, "ms");
-		// console.log("Znaleziony kontakt:");
-		// console.table(contacts);
+function invokeAction({ action, id, name, email, phone }) {
+	switch (action) {
+		case "list":
+			listContacts();
+			break;
 
-		// Usuwanie kontaktu
-		const contactIdToRemove = await prompt("Podaj nr id który chcesz usunąć: ");
-		const deleteStartTime = Date.now();
-		await removeContact(contactIdToRemove);
-		console.log("Czas usunięcia kontatku: ", Date.now() - deleteStartTime, "ms");
+		case "get":
+			getContactById(id);
+			break;
 
-		// Dodawanie kontaktów
-		const name = await prompt("Podaj swoje imię: ");
-		const email = await prompt("Podaj swój mail: ");
-		const phone = await prompt("Podaj swój numer telefonu: ");
+		case "add":
+			addContact(name, email, phone);
+			break;
 
-		const addStartTime = Date.now();
-		await addContact(name, email, phone);
-		console.log("Czas dodawani kontatku: ", Date.now() - addStartTime, "ms");
+		case "remove":
+			removeContact(id);
+			break;
 
-		rl.close();
-	} catch (error) {
-		console.log("Wystąpił błąd w teście: ", error.message);
+		default:
+			console.warn("\x1B[31m Unknown action type!");
 	}
 }
-test();
+
+invokeAction(argv);
